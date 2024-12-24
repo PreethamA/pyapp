d9faf33c90911e85ab64aac72823659b4f63abb7
PreethamA <preetham.armoor@gmail.com>
2024-12-24 17:58:24 +0100

    commit for correction of db

--- app/appy.py
+++ app/appy.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 import time
+import datetime as  dtime
 def val(i):
     try:
         conn = psycopg2.connect(
@@ -10,22 +11,32 @@ def val(i):
             password=os.environ.get("POSTGRES_PASSWORD"),
         )
         cur = conn.cursor()
-        k=f'user{i}@example.com'
-        cur.execute("INSERT INTO us (username, email) VALUES ('user1', %s)",(k,))  # Very simple query
+        k = f'user{i}@example.com'
+        #cur.execute("INSERT INTO us (username, email) VALUES ('user1', %s)",(k,))  # Very simple query
+        print(f"emailid:{k}")
         #result = cur.fetchone()
-        result = conn.commit()
-        print(f"Database connected and query executed: {result}")
-        cur.close()
-        conn.close()
+        #conn.commit()
+
+        cur.execute("""SELECT * FROM us""")
+        result1 = cur.fetchall()
+        for r in result1:
+            print(r)
+        #print(f"Database connected and query executed: {result1}")
+        #cur.close()
+        #conn.close()
 
     except psycopg2.Error as e:
         print(f"Database error: {e}")
     except Exception as e:
         print(f"General error: {e}")
-
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
 if __name__ == '__main__':
-#while True:
-    for i in range(103,200):
+  while True:
+    for i in range(303,500):
+        print(dtime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         time.sleep(10)
         val(i)
         print("App finished.") # Indicate normal exit
